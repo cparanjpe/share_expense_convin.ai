974b0a3ac46e1713f179864fad24aef937120998
Chaitanya Paranjpe <cparanjpe2003@gmail.com>
2024-10-20 13:54:05 +0530

    bug fix

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -3,7 +3,11 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET; // Use the same secret key
 
 exports.authenticate = (req, res, next) => {
-    const token = req.headers['authorization']; // Get token from the Authorization header
+    // Get token from the Authorization header
+    const token = req.headers['authorization'];
+    const {userId} = req.body;
+    const user_id = req.params?.id;
+    // console.log(userId,user_id);
 
     if (!token) {
         return res.status(401).json({ error: 'No token provided' });
@@ -13,7 +17,12 @@ exports.authenticate = (req, res, next) => {
         if (err) {
             return res.status(401).json({ error: 'Invalid token' });
         }
-        req.userId = decoded.id; // Attach user ID to the request
+        req.body.decoded_id = decoded.id;
+        if((userId && userId!="")|| ( user_id&& user_id!="") ){
+            if(userId && userId!="" && decoded.id!=userId)return res.status(401).json({ error: 'Unauthorized' });
+            if(user_id && user_id!="" && decoded.id!=user_id)return res.status(401).json({ error: 'Unauthorized' });
+        }
+            // Attach user ID to the request
         next(); // Proceed to the next route handler
     });
 };
